f75590dfa50b7a938b62a863b8bf9bca9e028b3e
Shulsfm <31509406+Shulsfm@users.noreply.github.com>
2019-04-27 21:19:12 +0300

    Update stage3.sh

--- stage3.sh
+++ stage3.sh
@@ -6,7 +6,14 @@ cd ~/Downloads
 echo 'Установка AUR (yay)'
 sudo pacman -Syu
 sudo pacman -S wget --noconfirm
-wget git.io/yay-install.sh && sh yay-install.sh --noconfirm
+
+#yaourt
+sudo pacman -S --noconfirm --needed wget curl git 
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si --skipinteg
+cd ..
+rm -rf yay-bin
 
 echo 'Создаем нужные директории'
 sudo pacman -S xdg-user-dirs --noconfirm
@@ -30,11 +37,7 @@ echo 'Скачать и установить конфиг и темы для XFC
 read -p "1 - Да, 0 - Нет: " xfce_set
 if [[ $xfce_set == 1 ]]; then
   echo 'Качаем и устанавливаем настройки Xfce'
-  # Чтобы сделать копию ваших настоек XFCE перейдите в домашнюю директорию ~/username открйте в этой категории терминал и выполните команду ниже.
-  # tar -czf xfce4.tar.gz .config/xfce4
-  # Выгрузите архив в интернет и скорректируйте ссылку на XFCE файл заменив ссылку на свою.
-  # wget git.io/xfce4.tar.gz
-  wget https://github.com/ordanax/backup/blob/master/xfce4.tar.gz?raw=true
+  wget https://github.com/Shulsfm/archsh/blob/master/files/xfce4.tar.gz
   sudo rm -rf ~/.config/xfce4/panel/
   sudo rm -rf ~/.config/xfce4/*
   sudo tar -xzf xfce4.tar.gz -C ~/
@@ -43,11 +46,11 @@ if [[ $xfce_set == 1 ]]; then
   sudo pacman -S capitaine-cursors
   
   echo 'Ставим лого ArchLinux в меню'
-  wget git.io/arch_logo.png
+  wget https://github.com/Shulsfm/archsh/blob/master/files/archlinux_logo.png
   sudo mv -f ~/Downloads/arch_logo.png /usr/share/pixmaps/arch_logo.png
 
   echo 'Ставим обои на рабочий стол'
-  wget git.io/bg.jpg
+  wget https://github.com/Shulsfm/archsh/blob/master/files/bg.jpg
   sudo rm -rf /usr/share/backgrounds/xfce/* #Удаляем стандартрые обои
   sudo mv -f ~/Downloads/bg.jpg /usr/share/backgrounds/xfce/bg.jpg
 elif [[ $xfce_set == 0 ]]; then
@@ -58,11 +61,7 @@ echo 'Скачать и установить конфиг и темы для Ope
 read -p "1 - Да, 0 - Нет: " openbox_set
 if [[ $openbox_set == 1 ]]; then
   echo 'Качаем и устанавливаем настройки Openbox'
-  #wget git.io/openbox.tar.gz
-  #sudo tar -xzf openbox.tar.gz -C ~/
-  #wget git.io/tint2.tar.gz
-  #sudo tar -xzf tint2.tar.gz -C ~/
-  wget https://github.com/ordanax/arch2018/raw/master/attach/config.tar.gz
+  wget https://github.com/Shulsfm/archsh/blob/master/files/config.tar.gz
   sudo tar -xzf config.tar.gz -C ~/
   yay -S obconf obmenu-generator obkey-git lxappearance-obconf tint2 nitrogen thunar mousepad wmctrl compton papirus-icon-theme
   sudo pacman -S capitaine-cursors
@@ -73,7 +72,7 @@ fi
 echo 'Убираем меню граб для выбора системы?'
 read -p "1 - Да, 0 - Нет: " grub_set
 if [[ $grub_set == 1 ]]; then
-  wget git.io/grub.tar.gz
+  wget https://github.com/Shulsfm/archsh/blob/master/files/grub.tar.gz
   sudo tar -xzf grub.tar.gz -C /
   sudo grub-mkconfig -o /boot/grub/grub.cfg
 elif [[ $grub_set == 0 ]]; then
@@ -84,7 +83,7 @@ echo 'Установить conky?'
 read -p "1 - Да, 0 - Нет: " conky_set
 if [[ $conky_set == 1 ]]; then
   sudo pacman -S conky conky-manager --noconfirm
-  wget git.io/conky.tar.gz
+  wget https://github.com/Shulsfm/archsh/blob/master/files/conky.tar.gz
   tar -xzf conky.tar.gz -C $HOME/
 elif [[ $conky_set == 0 ]]; then
   echo 'Установка conky пропущена.'
@@ -97,6 +96,6 @@ echo 'Добавляем в автозагрузку:'
 sudo systemctl enable ufw
 
 sudo rm -rf ~/Downloads
-sudo rm -rf ~/arch3.sh
+sudo rm -rf ~/stage3.sh
 
 echo 'Установка завершена!'
