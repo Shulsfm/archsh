50772a6a8585578cffb2490ee1a32f9426b65796
Shulsfm <31509406+Shulsfm@users.noreply.github.com>
2019-04-28 12:22:47 +0300

    Update stage1.sh

--- stage1.sh
+++ stage1.sh
@@ -11,26 +11,22 @@ echo '2.4 создание разделов'
   #boot
   echo n;
   echo;
-  echo;
+  echo 1;
   echo;
   echo +100M;
   #root
   echo n;
   echo;
+  echo 3;
   echo;
-  echo;
-  echo +15G;
+  echo +1024M;
   #swap
   echo n;
   echo;
-  echo;
-  echo +1024M;
-  echo;
-  #home 
-  echo n;
-  echo p;
+  echo 2;
   echo;
   echo;
+
   echo a;
   echo 1;
 
@@ -44,14 +40,12 @@ echo '2.4.2 Форматирование дисков'
 mkfs.ext2  /dev/sda1 -L boot
 mkfs.ext4  /dev/sda2 -L root
 mkswap /dev/sda3 -L swap
-mkfs.ext4  /dev/sda4 -L home
 
 echo '2.4.3 Монтирование дисков'
 mount /dev/sda2 /mnt
 mkdir /mnt/{boot,home}
 mount /dev/sda1 /mnt/boot
 swapon /dev/sda3
-mount /dev/sda4 /mnt/home
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
 echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
